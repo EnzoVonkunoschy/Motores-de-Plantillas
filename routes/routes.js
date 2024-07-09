8ea1109f6ce68eb4e812590da37174c70291abf4
enzovonkunoschy@gmail.com <enzovonkunoschy@gmail.com>
2024-07-09 19:20:36 -0300

    Con routes y router

---
+++ routes/routes.js
@@ -0,0 +1,15 @@
+// routes.js
+import { Router } from 'express';
+const router = Router();
+
+// Ruta para la página de inicio
+router.get('/', (req, res) => {
+  res.send('¡Bienvenido a nuestra página de inicio!');
+});
+
+// Ruta para la página de contacto
+router.get('/contacto', (req, res) => {
+  res.send('Ponte en contacto con nosotros aquí.');
+});
+
+export default router; // Exporta el router
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-var express = require('express')
+import express from 'express';
+
+//const routes = require('./routes');
+import router from './routes/routes.js'
 
 var app = express()
+app.use('/', router);
+/* 
+app.get('/',function(req,res){
+    res.send('Bienvenido a Tutoriales');
+});*/
 
-app.get('/',(req,res)=>{
-    res.send('Hola Mundo!')
-})
+app.get('/Node',function(req,res)
+{
+ res.send("Tutorial en Node");
+});
+app.get('/Angular',function(req,res)
+{
+ res.send("Tutorial en Angular");
+});
+
+
 
 var server = app.listen(3000,()=>{console.log("Escuchando en puerto")})
